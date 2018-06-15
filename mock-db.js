49087c64b7b37bfa61b951fb460bcfca2be7533a
hemanditwiz <hemanditwiz@gmail.com>
2018-06-15 19:27:47 +0530

    added mock collection

---
+++ mock-db.js
@@ -0,0 +1,7 @@
+const User = require('../models/UserModel')
+user = new User({name: 'Hemand', email: 'anand@gmail.com'})
+user.save(err => {
+  if(err){
+    console.log("Couldn't create user")
+  }
+})
